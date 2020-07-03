0bd618960939c69f091b96ae74cabacb5d9027df
Matthew Brett <matthew.brett@gmail.com>
2020-07-03 23:04:50 +0000

    Working instance, with https

--- vars.sh
+++ vars.sh
@@ -3,7 +3,9 @@ PROJECT_ID=uob-jupyterhub
 JHUB_CLUSTER=jhub-cluster
 RELEASE=jhub
 NAMESPACE=jhub
-JHUB_VERSION=0.9.0
+# See:
+# https://discourse.jupyter.org/t/trouble-getting-https-letsencrypt-working-with-0-9-0-beta-4/3583/5?u=matthew.brett
+JHUB_VERSION="0.9.0-n116.h1c766a1"
 REGION=europe-west2
 ZONE=europe-west2-b
 EMAIL=matthew.brett@gmail.com
