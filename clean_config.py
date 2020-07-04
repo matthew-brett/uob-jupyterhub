ab6a173e3d46180ecf0e82688c4dce9ff3d233ce
Matthew Brett <matthew.brett@gmail.com>
2020-07-04 13:41:49 +0100

    Config cleaning.

---
+++ clean_config.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+""" Clean config.yaml file of secrets
+
+Do a manual check after running this script.
+"""
+
+from random import choices
+from string import hexdigits
+
+from ruamel.yaml import YAML
+
+lowhex = [c for c in hexdigits if not c.isupper()]
+
+
+def is_hex(s, minlen=8):
+    if len(s) < minlen:
+        return False
+    return set(s).issubset(hexdigits)
+
+
+def rehex(s):
+    return ''.join(choices(lowhex, k=len(s)))
+
+
+def is_secret(key, val):
+    if 'secret' in key.lower():
+        return True
+    if key == 'tag':
+        return False
+    return is_hex(val)
+
+
+def clean_d(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            clean_d(v)
+            continue
+        elif isinstance(v, str) and is_secret(k, v):
+            v = rehex(v)
+        elif k == 'contactEmail':
+            v = 'yourname@somewhere.org'
+        d[k] = v
+
+
+yaml = YAML()
+with open('config.yaml', 'rt') as fobj:
+    config = yaml.load(fobj)
+
+
+clean_d(config)
+
+
+with open('config.yaml.cleaned', 'wt') as fobj:
+    fobj.write('# NB secret values replaced with random equivalents.\n'
+               '# Please check.\n\n')
+    yaml.dump(config, fobj)
