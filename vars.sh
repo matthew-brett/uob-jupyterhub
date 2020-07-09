91d6dbd3252a4d72cedc890612aacf8bd32eea1a
Matthew Brett <matthew.brett@gmail.com>
2020-07-09 12:25:54 +0000

    Vars for testing

--- vars.sh
+++ vars.sh
@@ -1,12 +1,12 @@
 # Source this file.
-PROJECT_ID=uob-jupyterhub
-JHUB_CLUSTER=jhub-cluster
-RELEASE=jhub
-NAMESPACE=jhub
+PROJECT_ID=uob-testing
+JHUB_CLUSTER=jhub-cluster-testing
+RELEASE=jhub-testing
+NAMESPACE=jhub-testing
 # See:
 # https://discourse.jupyter.org/t/trouble-getting-https-letsencrypt-working-with-0-9-0-beta-4/3583/5?u=matthew.brett
 JHUB_VERSION="0.9.0-n116.h1c766a1"
 REGION=europe-west2
 ZONE=europe-west2-b
 EMAIL=matthew.brett@gmail.com
-UOBHUB_IP=35.189.82.198
+UOBHUB_IP=34.105.129.229
