6db9d6dbea9936936d6355b8f8af5dae24131f7a
Matthew Brett <matthew.brett@gmail.com>
2020-09-08 17:30:23 +0100

    Allow for jupyterhub upgrade long pull times.

--- rehelm.sh
+++ rehelm.sh
@@ -4,9 +4,13 @@
 if [ -z "$JHUB_VERSION" ]; then
     . vars.sh
 fi
+# Timeout from:
+# https://zero-to-jupyterhub.readthedocs.io/en/latest/administrator/optimization.html#pulling-images-before-users-arrive
+# Allows pre-pulling of new Docker images on install / upgrade.
 helm upgrade $* \
     --cleanup-on-fail \
     $RELEASE jupyterhub/jupyterhub  \
+    --timeout 10m0s \
     --namespace=$NAMESPACE \
     --version=$JHUB_VERSION \
     --values config.yaml
