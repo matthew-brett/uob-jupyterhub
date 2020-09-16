06b474fc63e0a09ffa33c091aba2c2ee232d5ad5
Matthew Brett <matthew.brett@gmail.com>
2020-09-16 16:16:15 +0100

    Remove PD extra mount.

--- init_nfs.sh
+++ init_nfs.sh
@@ -5,7 +5,6 @@ source set_config.sh
 
 kubectl create namespace $NAMESPACE
 
-kubectl create -f configs/data_volume.yaml
 kubectl create -f nfs-configs/nfs_deployment.yaml
 kubectl create -f nfs-configs/nfs_service.yaml
 ./tools/kube_tpl_create.sh nfs-configs/nfs_pv_pvc_tpl.yaml
--- teardown_nfs.sh
+++ teardown_nfs.sh
@@ -5,8 +5,6 @@
 
 kubectl delete service nfs-server
 kubectl delete deployment nfs-server
-kubectl delete pvc pv-claim-demo
-kubectl delete pv pv-demo
 kubectl delete pvc nfs
 kubectl delete pv nfs
 kubectl delete pvc nfs-data
