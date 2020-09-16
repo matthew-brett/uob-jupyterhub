9914d16f3c1a41f3cdcbb087c415783795de94aa
Matthew Brett <matthew.brett@gmail.com>
2020-09-16 16:43:02 +0100

    Teardown into main script

--- teardown_everything.sh
+++ teardown_everything.sh
@@ -6,6 +6,14 @@
 . set_config.sh
 
 helm delete $RELEASE
+
+kubectl delete service nfs-server
+kubectl delete deployment nfs-server
+kubectl delete pvc nfs
+kubectl delete pv nfs
+kubectl delete pvc nfs-data
+kubectl delete pv nfs-data
+
 kubectl delete namespace $NAMESPACE
 gcloud container clusters delete $JHUB_CLUSTER --region $REGION --quiet
 
