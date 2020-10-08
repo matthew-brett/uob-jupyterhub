16bd89cb5c4d86ac491dd80699dfc54079367778
Matthew Brett <matthew.brett@gmail.com>
2020-10-09 00:12:16 +0100

    Remove unused storage classes

--- init_kubernetes.sh
+++ init_kubernetes.sh
@@ -12,11 +12,5 @@ kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole=cluster-admin \
   --user=$EMAIL
 
-# Initialize storage classes for SSD and standard storage
-# https://zero-to-jupyterhub.readthedocs.io/en/latest/customizing/user-storage.html
-# Needed for config.yaml setting of storage
-kubectl apply -f configs/pd_ssd.yaml
-kubectl apply -f configs/pd_std.yaml
-
 echo Next run
 echo source setup_helm.sh
