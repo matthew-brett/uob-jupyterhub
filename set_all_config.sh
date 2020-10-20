c7e9ab6309850ca6af9c42cb736f3685c8ac1a49
Matthew Brett <matthew.brett@gmail.com>
2020-10-20 12:03:37 +0100

    Make project_id explicit in various places
    Was getting errors of form:
    
    > The connection to the server <IP> was refused - did you specify the
    > right host or port?
    
    Make project explicit in various refresh commands.

---
+++ set_all_config.sh
@@ -0,0 +1,4 @@
+# Source this file
+source refresh_gcloud_auth.sh
+source set_config.sh
+source set_kconfig.sh
--- set_config.sh
+++ set_config.sh
@@ -4,6 +4,6 @@
 . vars.sh
 
 # Set gcloud parameters
-gcloud config set project $PROJECT_ID
+gcloud config set project ${PROJECT_ID}
 gcloud compute project-info add-metadata \
     --metadata google-compute-default-region=$REGION,google-compute-default-zone=$ZONE
